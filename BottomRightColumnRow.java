e9d4c51e5a77caedc5cfb107060c14bc03793acf
Tim Gesell <timothy.gesell@bvsd.org>
2024-08-30 15:19:36 -0600

    Add files via upload

---
+++ BottomRightColumnRow.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - 
+
+import static java.lang.System.*;
+
+public class BottomRightColumnRow implements Cipher
+{
+	private String[][] codeBox;
+
+	//method part of the public interface.  Should utilize private methods to do its work
+	public String encode(String original)
+	{
+		return "";
+	}
+
+	//method part of the public interface.  Should utilize private methods to do its work
+	public String decode(String encoded)
+	{
+		return "";
+	}
+
+	//instantiates an appropriately sized codeBox and fills it according to the original String
+	private void topLeftRowColumn(String original)
+	{
+	}
+
+	//returns the encoded String based on a correct transposition of the codeBox
+	private String botRightColumnRow()
+	{
+		return "";
+	}
+}
---
+++ Combined.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - 
+
+import static java.lang.System.*;
+
+public class Combined implements Cipher
+{
+	private Caesar cipher1;
+	private BottomRightColumnRow cipher2;
+
+	public Combined()
+	{
+		
+	}
+
+	public String encode(String toEncode)
+	{
+		return "";
+	}
+
+	public String decode(String toDecode)
+	{
+		return "";
+	}
+}
